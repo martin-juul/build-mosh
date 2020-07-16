39d1a399952dbe6cf1efc3675b2005f5324bb1e3
Martin Juul <martin@juul.xyz>
2020-07-16 15:17:05 +0200

    Update build-all.sh
    change url to fork

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd "${BASH_SOURCE%/*}"
-curl -OL https://github.com/blinksh/build-protobuf/releases/download/2.6.1/protobuf-2.6.1.tar.gz 
+curl -OL https://github.com/martin-juul/build-protobuf/releases/download/2.6.1/protobuf-2.6.1.tar.gz 
 
 tar -zxf protobuf-2.6.1.tar.gz || { echo "Protobuf download error"; exit 1; }
 mv -f protobuf-2.6.1/ protobuf-release
